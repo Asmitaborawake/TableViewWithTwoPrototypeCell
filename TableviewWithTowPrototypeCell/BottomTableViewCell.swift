aa54e870bbe6529239bfd2ed4abea4649fcffb00
Asmita Borawake <asmita.borawake@Sneha-Sures-Mac.local>
2019-10-03 17:36:30 +0530

    first commit

---
+++ TableviewWithTowPrototypeCell/BottomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BottomTableViewCell.swift
+//  TableviewWithTowPrototypeCell
+//
+//  Created by Asmita Borawake on 03/10/19.
+//  Copyright © 2019 Asmita Borawake. All rights reserved.
+//
+
+import UIKit
+
+class BottomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TableviewWithTowPrototypeCell/TopTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TopTableViewCell.swift
+//  TableviewWithTowPrototypeCell
+//
+//  Created by Asmita Borawake on 03/10/19.
+//  Copyright © 2019 Asmita Borawake. All rights reserved.
+//
+
+import UIKit
+
+class TopTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
