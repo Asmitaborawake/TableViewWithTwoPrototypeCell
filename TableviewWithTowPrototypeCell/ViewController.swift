5e6f505f71089080c3124f55a6909e4218a1d271
Asmita Borawake <asmita.borawake@sneha-sures-mac.local>
2019-10-03 17:01:18 +0530

    Initial Commit

---
+++ TableviewWithTowPrototypeCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableviewWithTowPrototypeCell
+//
+//  Created by Asmita Borawake on 03/10/19.
+//  Copyright © 2019 Asmita Borawake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
